e5026314058198f417072a916e7139f865b396b2
BillieR121 <billie916@gmail.com>
2021-01-20 17:30:45 -0600

    added controllers and models for current database

---
+++ app/Http/Controllers/DivisionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Divisions;
+use Illuminate\Http\Request;
+
+class DivisionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Divisions  $divisions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Divisions $divisions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Divisions  $divisions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Divisions $divisions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Divisions  $divisions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Divisions $divisions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Divisions  $divisions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Divisions $divisions)
+    {
+        //
+    }
+}
