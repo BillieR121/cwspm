61e43321fb402f8bd3d6f3e5b1c52067a7f18eb8
BillieR121 <billie916@gmail.com>
2021-02-06 17:22:18 -0600

    Just updating minor changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="grid-container full">
             <div class="grid-x">
-                <h1>Admin Home Page</h1>
+                <h1 class="subheader">Admin Home Page</h1>
             </div>
         </div>
     </div>
